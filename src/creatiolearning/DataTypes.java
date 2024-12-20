8336c1fe87eb497fc8daf14cb9e7be6577856dd4
Manasa Uddisapu <manasa.uddisapu@outlook.com>
2024-12-20 23:14:50 +0530

    Assignment1 Datatypes

---
+++ src/creatiolearning/DataTypes.java
@@ -0,0 +1,19 @@
+package creatiolearning;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int x=1;
+		long y=12345678;
+		float f=0.1f;
+		double d=2.39886d;
+		char c='A';
+		
+		String S="Manasa";
+		
+		System.out.println(x);
+		
+
+	}
+
+}
